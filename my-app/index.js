64cf32c8182d4901a8f98589fa0f14b84909cd1e
aldehf420 <aldehf420@naver.com>
2019-06-07 02:09:05 +0000

    tt

--- my-app/index.js
+++ my-app/index.js
@@ -17,8 +17,8 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
 	host: 'localhost',
 	user: 'root',
-	post: 3307,
-	password: '',
+	post: 3306,
+	password: '1234',
 	database: 'my_db'
 });
 
